e6d01fc479d16605b161b5cb763b722d4a4f11c4
Maria Sarnova <monicamonica@MacBook-Pro-Monica.local>
2021-03-12 21:32:32 +0300

    visitors, orederZone and cashier are added

---
+++ src/main/java/com/epam/task/sixth/entities/Visitors.java
@@ -0,0 +1,12 @@
+package com.epam.task.sixth.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Visitors {
+    private List<Visitor> visitors=new ArrayList<>();
+
+    public List<Visitor> getVisitors() {
+        return visitors;
+    }
+}
